1b6496521a72c3e48e0b4688cbd1f3e9c97db097
Al-aminhossai1 <md.al.aminhossain1785@gmail.com>
2022-03-11 01:26:31 +0600

    Update Liked Posts

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ const switchTab = (id) => {
     document.getElementById("liked").style.display = "none";
     document.getElementById("reported").style.display = "none";
   } else if (id === "liked") {
+    document.getElementById("liked").textContent = "";
     document.getElementById("liked").style.display = "block";
     document.getElementById("posts").style.display = "none";
     document.getElementById("reported").style.display = "none";
@@ -172,3 +173,4 @@ const loadPosts = async () => {
 }
 
 loadPosts();
+switchTab
